22d37e9c340268c03cd25d14d5169333534989ad
JiroDavid <jirodavid153@gmail.com>
2026-09-16 20:09:30 +0100

    test: cover product wider than prime header

--- test/formatTable.test.js
+++ test/formatTable.test.js
@@ -8,3 +8,10 @@ test('formatTable([2, 3], [[4, 6], [6, 9]]) formats as aligned grid', () => {
         "  2 3\n-----\n2 4 6\n3 6 9"
     );
 });
+
+test('formatTable([7, 11], [[49, 77], [77, 121]]) widens columns to fit a product wider than any prime', () => {
+    assert.strictEqual(
+        formatTable([7, 11], [[49, 77], [77, 121]]),
+        "      7  11\n-----------\n  7  49  77\n 11  77 121"
+    );
+});
